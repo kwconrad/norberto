903c35b5f539f36e459cf63aa47ff3f47ae5fe0e
Kyle Conrad <kwconrad@protonmail.com>
2025-02-17 16:27:26 +0000

    render single note with markdown formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen w-screen overflow-auto flex items-center justify-center">
       <main className="flex flex-col">
-        <Note id={0} body={"hello **world**"}></Note>
+        <Note id={0} body={"hello **world**"} isActiveNote={true}></Note>
       </main>
     </div>
   );
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,27 +1,37 @@
 "use client";
+import { marked } from "marked";
 import { useState } from "react";
 
 type NoteProps = {
   id: number;
   body: string;
+  isActiveNote: boolean;
 };
 
-export default function Note({ id, body }: NoteProps) {
+export default function Note({ id, body, isActiveNote }: NoteProps) {
   const [text, setText] = useState(body);
   const changeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.target.value);
   };
 
+  const createMarkdown = () => {
+    return { __html: marked(text) };
+  };
+
   return (
     <div className="w-80 aspect-square bg-white shadow-lg rounded-lg">
       <div className="p-4 text-neutral-800 w-full min-h-full">
-        <textarea
-          id={`note-${id}`}
-          name={`note-${id}`}
-          value={text}
-          onChange={changeText}
-          className="focus:outline-none w-full min-h-full text-neutral-800 h-auto resize-none"
-        ></textarea>
+        {isActiveNote ? (
+          <textarea
+            id={`note-${id}`}
+            name={`note-${id}`}
+            value={text}
+            onChange={changeText}
+            className="focus:outline-none w-full min-h-full text-neutral-800 h-auto resize-none"
+          ></textarea>
+        ) : (
+          <div dangerouslySetInnerHTML={createMarkdown()}></div>
+        )}
       </div>
     </div>
   );
