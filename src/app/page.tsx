1a28b2ea93c60b346e36f9f45985ddceb555371c
Kyle Conrad <kwconrad@protonmail.com>
2025-02-21 11:20:25 +0000

    Handle redirect in base page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+
+export default async function Profile({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  if (!id) {
+    redirect(`/session/${uuidv4()}`);
+  }
+
   return null;
 }
